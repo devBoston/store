794a11945b16a403eed42c99a03fb89fc7d7a3dc
ssuchir <ssuchir@gmail.com>
2021-02-20 20:06:31 -0500

    updating with shop page, collection preview, collection item with images rendering

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
 import HomePage from './pages/homepage/hompage.component';
+import ShopPage from './pages/shop/shop.component'
 
 import {Switch, Route} from 'react-router-dom';
 
 import './App.css';
 
 
-const HatsPage = () => (
-    <div>
-       <h1>
-       hi hat page
-           </h1> 
-    </div>
-)
-
 function App() {
     return (
         <div>
             <Switch>
             <Route exact='/' path component={HomePage}/>
-            <Route path='/hats'  component={HatsPage}/>
+            <Route path='/shop'  component={ShopPage}/>
             </Switch>
         </div>
     );
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,15 +1,20 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { withRouter } from "react-router-dom";
 
-import './menu-item.styles.scss';
+import "./menu-item.styles.scss";
 
-const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
-    <div    
-        className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-    <div className='background-image' style ={{ backgroundImage: `url(${imageUrl})` }}/>
-    <div className='content'>
-<div className='title'>{title.toUpperCase()}</div>
-      <span className='subtitle'>SHOP NOW</span>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className="background-image"
+      style={{ backgroundImage: `url(${imageUrl})` }}
+    />
+    <div className="content">
+      <div className="title">{title.toUpperCase()}</div>
+      <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
