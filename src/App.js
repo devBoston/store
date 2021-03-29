65e38815ff549269489676a2026b14920280b18b
ssuchir <ssuchir@gmail.com>
2021-03-29 18:24:31 -0500

    finished sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.com
 
 import { Switch, Route } from "react-router-dom";
 
-import { auth,createUserProfileDocument } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 import "./App.css";
 
@@ -22,25 +22,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
+        userRef.onSnapshot((snapShot) => {
           this.setState({
             currentUser: {
               id: snapShot.id,
-              ...snapShot.data()
-            }
+              ...snapShot.data(),
+            },
           });
-
-          console.log(this.state);
         });
+      } else {
+        this.setState({ currentUser: userAuth });
       }
-        else {
-          this.setState({ currentUser: userAuth });
-        }
-    
     });
   }
 
