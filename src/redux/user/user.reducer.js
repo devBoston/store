eb43676344f09fc9de4d8f67f225e52ba3c4b509
ssuchir <ssuchir@gmail.com>
2021-04-13 16:19:39 -0500

    added cart icon and dropdown components. implemented cart reducer

--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -6,7 +6,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
-    case "SET_CURRENT_USER":
+    case UserActionTypes.SET_CURRENT_USER:
       return {
         ...state,
         currentUser: action.payload,
